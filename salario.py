5a31854cb8ee7c1d1bac54c8fe804fa316e3f83d
Bielz77 <165225445+Bielz77@users.noreply.github.com>
2024-05-17 05:55:19 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,6 @@
+# Salário atual do usuário mais 5%
+
+salario = float(input('Dgite o seu salário atual: '))
+porcentagem = (salario * 5) /100
+salario_p = (porcentagem+salario)
+print("Seu salário com 5 porcento de comissão é:", salario_p)
